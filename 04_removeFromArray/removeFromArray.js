10d4704df644ea0bbdd36153dbfd3049254bb2c8
victor-schumann <schumann@tutamail.com>
2022-09-25 01:24:18 +0100

    exercise 4 attempt

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,18 @@
 const removeFromArray = function(arr, value) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 'value') {
+        newArr.push(arr[i]);
+    }
+}
+    
+    /* let i = value.lenght;
     return arr.filter(function(ele) {
         return ele != value;
-    })
+    }) */
+
+    //try use the lenght of the amount of values to keep the code running
+    //maybe try splice + forEach or filter().
 };
 
 // Do not edit below this line
