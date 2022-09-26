f34b7fc06f231b5865f36dba595a245ca2c3721a
victor-schumann <schumann@tutamail.com>
2022-09-26 19:54:03 +0100

    Failed offline attempts

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,45 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
 
+    Temos o primeiro número;
+    Soma = primeiro número;
+    Se soma for menor ou igual ao segundo número:
+        soma = soma + 1;
+    retornar soma.
+
+    let sum = first;
+    if (sum < second) {
+        return sum + 1;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+3 e 6
+3 + 4 + 5 + 6
+
+soma = x
+se soma 
+
+x + 1
+x + 2
+x + 3
+
+
+
+
+x + 2
+x + 3
+x + 4
+x + 5
+
+x + 4
+x + 5
+x + 6
+x + 7
+x + 8
+
+3x + 2*3
+4x + (4+3*2
+5x + 2*3*5
