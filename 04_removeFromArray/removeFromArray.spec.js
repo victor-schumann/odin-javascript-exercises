8412169d0ceed5a4bd6d562517a7467365553cc6
victor-schumann <schumann@tutamail.com>
2022-09-26 20:34:59 +0100

    Debugging

--- 04_removeFromArray/removeFromArray.spec.js
+++ 04_removeFromArray/removeFromArray.spec.js
@@ -1,13 +1,13 @@
 const removeFromArray = require('./removeFromArray')
 
 describe('removeFromArray', () => {
-  test('removes a single value', () => {
+  tests('removes a single value', () => {
     expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
-  test('removes multiple values', () => {
+  test.skip('removes multiple values', () => {
     expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
   });
-  test('ignores non present values', () => {
+  test.skip('ignores non present values', () => {
     expect(removeFromArray([1, 2, 3, 4], 7, "tacos")).toEqual([1, 2, 3, 4]);
   });
   test.skip('ignores non present values, but still works', () => {
--- [ok]_repeatString/repeatString.js
+++ [ok]_repeatString/repeatString.js
@@ -2,13 +2,26 @@
 repeatString('hey', 3) // returns 'heyheyhey' */
 
 const repeatString = function(str, num) {
-    let repeatedStr = "";
-    while (num > 0) {
-        repeatedStr += str;
-        num --;
-    }
-    return repeatedStr;
+    let repeatedStr = str;
 
+    if (num === 1) {
+    return str;
+
+    } else if (num < 0){
+        return "ERROR";
+
+    } else if (num > 1) {
+        while (num > 1) {
+            repeatedStr += str;
+            num --;
+
+        }
+        return repeatedStr;
+
+    } else if (num === 0) {
+        return '';
+
+    }
 };
 
 // Do not edit below this line
